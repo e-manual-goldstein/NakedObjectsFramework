31842e62e849f3acf3ce14b36ff09aecdddaff90
Richard Pawson <rpawson@nakedobjects.net>
2019-07-17 09:16:55 +0100

    Moved helper functions from AW model to FunctionalProgrammingModel

---
+++ Functional Programming Model/NakedFunctions.Helpers/CloneHelpers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Remutable;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace NakedFunctions
+{
+    public static class CloneHelpers
+    {
+
+
+        public static TInstance With<TInstance, TValue>(
+            this TInstance source,
+            Expression<Func<TInstance, TValue>> expression,
+            TValue value)
+        {
+
+            var instanceType = source.GetType();
+
+            var cc = instanceType.GetConstructors().Single(c => c.GetParameters().Any<ParameterInfo>());
+
+            var config = new ActivationConfiguration().Configure(cc);
+
+            var rm = new Remute(config);
+
+            return rm.With(source, expression, value);
+        }
+    }
+}
