4b3ce46c2a3e09bd59d89c53c5504ee17fb86952
Richard Pawson <rpawson@nakedobjects.net>
2019-07-19 14:43:26 +0100

    Added ReturnHelpers

---
+++ Functional Programming Model/NakedFunctions.Helpers/ReturnHelpers.cs
@@ -0,0 +1,41 @@
+﻿namespace NakedFunctions
+{
+    /// <summary>
+    /// Helper functions for returning items from action functions
+    /// </summary>
+    /// 
+    public static class ReturnHelpers
+    {
+        //TODO: return type of 'object' in Tuples should be converted to '?' in C# 8
+
+        public static T Display<T>(T obj) 
+        {
+            return obj;
+        }
+
+        public static (T, object, string) Display<T>(T toDisplay, string withMessage)
+        {
+            return (toDisplay, null, withMessage);
+        }
+
+        public static (object,T) DisplayAndPersistSameItem<T>(T toDisplay)
+        {
+            return (null, toDisplay);
+        }
+
+        public static (object,T, string) DisplayAndPersistSameItem<T>(T toDisplayAndPersist, string withMessage)
+        {
+            return (null, toDisplayAndPersist, withMessage);
+        }
+
+        public static (T, U) DisplayAndPersistDifferentItems<T, U>(T toDisplay, U toPersist)
+        {
+            return (toDisplay, toPersist);
+        }
+
+        public static (T, U, string) DisplayAndPersistDifferentItems<T,U>(T toDisplay, U toPersist, string withMessage)
+        {
+            return (toDisplay, toPersist, withMessage);
+        }
+    }
+}
