df5de2d24c705cfae4cfea215d5cf9151c0d739b
Richard Pawson <rpawson@nakedobjects.net>
2019-07-24 11:02:36 +0100

    Update ReturnHelpers.cs

--- Functional Programming Model/NakedFunctions.Helpers/ReturnHelpers.cs
+++ Functional Programming Model/NakedFunctions.Helpers/ReturnHelpers.cs
@@ -11,32 +11,32 @@ namespace NakedFunctions {
     /// </summary>
     /// 
     public static class Result {
-        //TODO: return type of 'object' in Tuples should be converted to '?' in C# 8
-        public static (object, object, string) MessageOnly(string message) {
-            return (null, null, message);
+
+        public static string MessageOnly(string message) {
+            return message;
         }
 
-        public static T ToDisplay<T>(T item) {
+        public static T Display<T>(T item) {
             return item;
         }
 
-        public static (T, object, string) ToDisplay<T>(T item, string withMessage) {
-            return (item, null, withMessage);
+        public static (T, string) Display<T>(T item, string withMessage) {
+            return (item, withMessage);
         }
 
-        public static (object, T) ToPersistAndDisplay<T>(T item) {
-            return (null, item);
+        public static (T, T) DisplayAndPersist<T>(T item) {
+            return (item, item);
         }
 
-        public static (object, T, string) ToPersistAndDisplay<T>(T item, string withMessage) {
-            return (null, item, withMessage);
+        public static (T, T, string) DisplayAndPersist<T>(T item, string withMessage) {
+            return (item, item, withMessage);
         }
 
-        public static (T, U) ToDisplayAndPersistDifferentItems<T, U>(T itemToDisplay, U toPersist) {
+        public static (T, U) DisplayAndPersistDifferentItems<T, U>(T itemToDisplay, U toPersist) {
             return (itemToDisplay, toPersist);
         }
 
-        public static (T, U, string) ToDisplayAndPersistDifferentItems<T, U>(T itemToDisplay, U toPersist, string withMessage) {
+        public static (T, U, string) DisplayAndPersistDifferentItems<T, U>(T itemToDisplay, U toPersist, string withMessage) {
             return (itemToDisplay, toPersist, withMessage);
         }
     }
