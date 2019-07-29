4c7548a0be4722f906f880d69f5ed57bbdcde829
Richard Pawson <rpawson@nakedobjects.net>
2019-07-29 18:58:18 +0100

    Added view model interfaces, and MainMenu types to replace FunctionalPlaceholder

---
+++ Functional Programming Model/NakedFunctions.Helpers/MainMenu.cs
@@ -0,0 +1,8 @@
+﻿namespace NakedFunctions
+{
+    //Temporary solution to providing a default instantiable object on which
+    //main menu actions are located for purposes of the RO resource URL.
+    public class MainMenu
+    {
+    }
+}
