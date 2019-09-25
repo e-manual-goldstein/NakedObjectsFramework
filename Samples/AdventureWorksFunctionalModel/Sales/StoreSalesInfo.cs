64cca6cbf549e74cabfbd856fcd308109b381092
scascarini <scascarini@nakedobjects.org>
2019-09-25 18:14:46 +0100

    Update StoreSalesInfo.cs

--- Samples/AdventureWorksFunctionalModel/Sales/StoreSalesInfo.cs
+++ Samples/AdventureWorksFunctionalModel/Sales/StoreSalesInfo.cs
@@ -1,22 +1,26 @@
-﻿using NakedFunctions;
+﻿// Copyright Naked Objects Group Ltd, 45 Station Road, Henley on Thames, UK, RG9 1AT
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and limitations under the License.
+
+using System.Linq;
+using NakedFunctions;
 using NakedObjects;
 using static NakedFunctions.Result;
-using System.Linq;
 
-namespace AdventureWorksModel
-{
+namespace AdventureWorksModel {
     //TODO: Need to think how we want to do ViewModels. Can't require methods to be implemented.
     //Probably just need a constructor that takes all keys, as well as any required Injected params
     [ViewModel]
-    public class StoreSalesInfo
-    {
+    public class StoreSalesInfo {
         public StoreSalesInfo(
-            string accountNumber, 
+            string accountNumber,
             bool editMode,
             string storeName,
             SalesTerritory salesTerritory,
-            SalesPerson salesPerson)
-        {
+            SalesPerson salesPerson) {
             AccountNumber = accountNumber;
             StoreName = storeName;
             SalesTerritory = salesTerritory;
@@ -24,64 +28,48 @@ namespace AdventureWorksModel
             EditMode = editMode;
         }
 
-        public StoreSalesInfo()
-        {
-
-        }
+        public StoreSalesInfo() { }
 
         [MemberOrder(1), Disabled]
-        public  string AccountNumber { get; set; }
+        public string AccountNumber { get; set; }
 
         [MemberOrder(2)]
-        public  string StoreName { get; set; }
+        public string StoreName { get; set; }
 
         [MemberOrder(3)]
-        public  SalesTerritory SalesTerritory { get; set; }
+        public SalesTerritory SalesTerritory { get; set; }
 
         [MemberOrder(4)]
-        public  SalesPerson SalesPerson { get; set; }
-
+        public SalesPerson SalesPerson { get; set; }
 
         public bool EditMode { get; set; }
-
     }
 
-    public static class StoreSalesInfoFunctions
-    {
-
-        public static string Title(
-            StoreSalesInfo vm,
-            [Injected] IQueryable<Customer> customers)
-        {
+    public static class StoreSalesInfoFunctions {
+        public static string Title(this StoreSalesInfo vm,
+                                   [Injected] IQueryable<Customer> customers) {
             return $"{(vm.EditMode ? "Editing - " : "")} Sales Info for: {vm.StoreName}";
         }
 
-
-        public static string[] DeriveKeys(StoreSalesInfo vm)
-        {
-            return new string[] { vm.AccountNumber, vm.EditMode.ToString() };
+        public static string[] DeriveKeys(StoreSalesInfo vm) {
+            return new string[] {vm.AccountNumber, vm.EditMode.ToString()};
         }
 
-        public static StoreSalesInfo PopulateUsingKeys(
-            StoreSalesInfo vm,
-            string[] keys,
-            [Injected] IQueryable<Customer> customers)
-        {
+        public static StoreSalesInfo PopulateUsingKeys(StoreSalesInfo vm,
+                                                       string[] keys,
+                                                       [Injected] IQueryable<Customer> customers) {
             var cus = CustomerRepository.FindCustomerByAccountNumber(keys[0], customers).Item1;
             return vm.With(x => x.AccountNumber, keys[0])
                 .With(x => x.SalesTerritory, cus.SalesTerritory)
-                .With(x => x.SalesPerson, cus.Store.SalesPerson)
+                .With(x => x.SalesPerson, cus.Store?.SalesPerson)
                 .With(x => x.EditMode, bool.Parse(keys[1]));
         }
 
-        public static StoreSalesInfo Edit(StoreSalesInfo ssi)
-        {
+        public static StoreSalesInfo Edit(this StoreSalesInfo ssi) {
             return ssi.With(x => x.EditMode, true);
         }
 
-
-        public static bool HideEdit(StoreSalesInfo ssi)
-        {
+        public static bool HideEdit(this StoreSalesInfo ssi) {
             return IsEditView(ssi);
         }
 
@@ -90,10 +78,8 @@ namespace AdventureWorksModel
             return ssi.EditMode;
         }
 
-        public static (Customer, Customer) Save(
-            StoreSalesInfo vm,
-            [Injected] IQueryable<Customer> customers)
-        {
+        public static (Customer, Customer) Save(this StoreSalesInfo vm,
+                                                [Injected] IQueryable<Customer> customers) {
             var (cus, _) = CustomerRepository.FindCustomerByAccountNumber(vm.AccountNumber, customers);
             var st = vm.SalesTerritory;
             var sp = vm.SalesPerson;
@@ -105,10 +91,7 @@ namespace AdventureWorksModel
             return DisplayAndPersist(cus2);
         }
 
-
-        public static bool HideSave(
-            StoreSalesInfo ssi)
-        {
+        public static bool HideSave(this StoreSalesInfo ssi) {
             return !IsEditView(ssi);
         }
     }
